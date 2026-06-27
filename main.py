d3b7e4a906191565a78cad172a660adb73c70257
Thiago Hass <thiagohass2@gmail.com>
2026-06-26 23:41:59 -0300

    Atualiza main.py com envio de imagens

--- main.py
+++ main.py
@@ -16,22 +16,39 @@ def run_flask():
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+# lista de mensagens com texto + foto
 mensagens = [
-    "Mensagem 1",
-    "Mensagem 2",
-    "Mensagem 3",
-    "Mensagem 4",
-    "Mensagem 5"
+    {"texto": "_*🍛 NÃO ESQUECE DE GRAVAR A LOUÇA DO ALMOÇO! 🧼*_
+
+Garanta aqueles minutos valiosos para suas horas diárias.", "foto": "https://drive.google.com/file/d/1mwuUsvMMXb3LUC-VWyOUfKvcLv0ZpTXy/view"},
+    {"texto": "_*👯‍♀️ CHAMA AS AMIGAS! 👯‍♀️*_
+`Coisa boa precisa ser dividida com quem você gosta!` 
+
+Traga suas amigas para a campanha de Tarefas Domésticas e *ganhem juntas*! ", "foto": "https://drive.google.com/file/d/1im_AEQ6WzxPNe4OtwG2mXRebNXvJ_MDy/view"},
+    {"texto": "_*👫 TRAGA SEUS AMIGOS E GANHE PELAS HORAS QUE ELES GRAVAREM 👫*_
+`Compartilhe seu link e ganhe pelas horas que seus indicados gravarem. `", "foto": "_*👫 TRAGA SEUS AMIGOS E GANHE PELAS HORAS QUE ELES GRAVAREM 👫*_
+`Compartilhe seu link e ganhe pelas horas que seus indicados gravarem. `"},
+    {"texto": "Mensagem 4", "foto": "https://exemplo.com/foto4.jpg"},
+    {"texto": "Mensagem 5", "foto": "https://exemplo.com/foto5.jpg"},
 ]
 
 indice = 0
 
 async def enviar_mensagem(context):
     global indice
+    msg = mensagens[indice]
 
+    # envia texto
     await context.bot.send_message(
         chat_id=CHAT_ID,
-        text=mensagens[indice]
+        text=msg["texto"]
+    )
+
+    # envia foto
+    await context.bot.send_photo(
+        chat_id=CHAT_ID,
+        photo=msg["foto"],
+        caption=f"Imagem da {msg['texto']}"
     )
 
     indice = (indice + 1) % len(mensagens)
@@ -39,7 +56,6 @@ async def enviar_mensagem(context):
 def main():
     if not TOKEN:
         raise ValueError("TOKEN não encontrada.")
-
     if not CHAT_ID:
         raise ValueError("CHAT_ID não encontrado.")
 
